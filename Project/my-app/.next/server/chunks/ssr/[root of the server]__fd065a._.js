96d72bb3c9c137f35a3a4b1f7c41343b3efeb184
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-05-24 23:35:37 +0300

    Display book info in page.jsx

--- Project/my-app/.next/server/chunks/ssr/[root of the server]__fd065a._.js
+++ Project/my-app/.next/server/chunks/ssr/[root of the server]__fd065a._.js
@@ -8201,22 +8201,27 @@ __turbopack_esm__({
     "default": (()=>Home)
 });
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
-var __TURBOPACK__imported__module__$5b$project$5d2f$repo$2f$prisma$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/repo/prisma.js [app-rsc] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$ui$2f$Card$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/ui/Card.js [app-rsc] (ecmascript)");
 ;
 ;
-;
-function Home() {
+async function Home() {
+    const books = await (0, __TURBOPACK__imported__module__$5b$project$5d2f$ui$2f$Card$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["getAllBooks"])();
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Fragment"], {
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-            children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("getAllBooks", {}, void 0, false, {
-                fileName: "[project]/app/page.jsx",
-                lineNumber: 8,
-                columnNumber: 9
-            }, this)
+            children: books.map((book)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                    children: [
+                        book.name,
+                        ",,,,",
+                        book.author
+                    ]
+                }, book.id, true, {
+                    fileName: "[project]/app/page.jsx",
+                    lineNumber: 11,
+                    columnNumber: 11
+                }, this))
         }, void 0, false, {
             fileName: "[project]/app/page.jsx",
-            lineNumber: 7,
+            lineNumber: 9,
             columnNumber: 7
         }, this)
     }, void 0, false);
--- Project/my-app/app/page.jsx
+++ Project/my-app/app/page.jsx
@@ -1,11 +1,18 @@
-import prisma from "@/repo/prisma";
 import { getAllBooks } from '../ui/Card.js';
 
-export default function Home() {
+export default async function Home() {
+
+  const books = await getAllBooks();
+
   return (
     <>
       <div>
-        <getAllBooks />
+        {books.map((book) => (
+          <li key={book.id}>
+              {book.name},,,,
+              {book.author}
+          </li>
+        ))}
       </div>
     </>
   );
