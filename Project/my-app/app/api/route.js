c38d14cfa71bf3394fe766847294ea3f9d7fc566
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-05-28 19:30:45 +0300

    Add route.js

--- Project/my-app/app/api/route.js
+++ Project/my-app/app/api/route.js
@@ -1,2 +1,21 @@
 import { Prisma } from "@/prisma/client";
+import { NextResponse } from "next/server";
 
+
+export default async function POST(request) {
+    
+    try {
+
+        const body = await request.json();
+        const { name, author, status } = body;
+
+        if (!name || !author || !status) {
+            return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: 'Server error' }, {status: 500});
+    }
+
+}
